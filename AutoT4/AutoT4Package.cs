f71b323e7db1b4f69744ab2031d0b580ecf056aa
Grimace of Despair <grimaceofdespair@hotmail.com>
2019-05-04 08:53:58 +0200

    Support VS2019 (note: backwards compatibility ignored!)

--- AutoT4/AutoT4Package.cs
+++ AutoT4/AutoT4Package.cs
@@ -7,91 +7,91 @@ using System.Linq;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace BennorMcCarthy.AutoT4
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
     [Guid(GuidList.guidAutoT4PkgString)]
-    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideOptionPage(typeof(Options), Options.CategoryName, Options.PageName, 1000, 1001, false)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    public sealed class AutoT4Package : Package
+    public sealed class AutoT4Package : AsyncPackage
     {
         private const int CommandId = 0x0100;
         private static readonly Guid CommandSet = new Guid("92c31ba2-5827-4779-b3ff-cf0fed43e50a");
 
         private DTE _dte;
-        private BuildEvents _buildEvents;
-        private ObjectExtenders _objectExtenders;
-        private AutoT4ExtenderProvider _extenderProvider;
-        private readonly List<int> _extenderProviderCookies = new List<int>();
+        Tuple<_dispBuildEvents_OnBuildBeginEventHandler, _dispBuildEvents_OnBuildDoneEventHandler> _buildEvents;
 
         private Options Options
         {
             get { return (Options)GetDialogPage(typeof(Options)); }
         }
 
-        protected override void Initialize()
+        protected async override System.Threading.Tasks.Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             base.Initialize();
 
-            _dte = GetService(typeof(SDTE)) as DTE;
+            _dte = await GetServiceAsync(typeof(EnvDTE.DTE)) as EnvDTE.DTE;
             if (_dte == null)
-                return;
+            {
+                throw new NotImplementedException("AutoT4 must run within Visual Studio");
+            }
 
-            RegisterExtenderProvider(VSConstants.CATID.CSharpFileProperties_string);
-            RegisterExtenderProvider(VSConstants.CATID.VBFileProperties_string);
+            await RegisterT4Files(_dte);
 
-            RegisterEvents();
+            _buildEvents = RegisterEvents(_dte);
 
             var commandService = (IMenuCommandService)GetService(typeof(IMenuCommandService));
             var menuCommandID = new CommandID(CommandSet, CommandId);
-            var menuItem = new MenuCommand(this.RunTemplates, menuCommandID);
+            var menuItem = new MenuCommand((object sender, EventArgs e) => this.RunTemplates(_dte), menuCommandID);
             commandService.AddCommand(menuItem);
         }
 
-        private void RegisterEvents()
+        private async System.Threading.Tasks.Task RegisterT4Files(DTE dte)
         {
-            _buildEvents = _dte.Events.BuildEvents;
-            _buildEvents.OnBuildBegin += OnBuildBegin;
-            _buildEvents.OnBuildDone += OnBuildDone;
+            var objectExtenders = await GetServiceAsync(typeof(ObjectExtenders)) as ObjectExtenders;
+            var extenderProvider = new AutoT4ExtenderProvider(dte);
+            objectExtenders.RegisterExtenderProvider(VSConstants.CATID.CSharpFileProperties_string, AutoT4ExtenderProvider.Name, extenderProvider);
+            objectExtenders.RegisterExtenderProvider(VSConstants.CATID.VBFileProperties_string, AutoT4ExtenderProvider.Name, extenderProvider);
         }
 
-        private void RegisterExtenderProvider(string catId)
+        private Tuple<_dispBuildEvents_OnBuildBeginEventHandler, _dispBuildEvents_OnBuildDoneEventHandler> RegisterEvents(DTE dte)
         {
-            const string name = AutoT4ExtenderProvider.Name;
+            _dispBuildEvents_OnBuildBeginEventHandler buildBegin = (scope, action) => OnBuildBegin(dte, scope);
+            _dispBuildEvents_OnBuildDoneEventHandler buildDone = (scope, action) => OnBuildDone(dte, scope);
 
-            _objectExtenders = _objectExtenders ?? GetService(typeof(ObjectExtenders)) as ObjectExtenders;
-            if (_objectExtenders == null)
-                return;
+            dte.Events.BuildEvents.OnBuildBegin += buildBegin;
+            dte.Events.BuildEvents.OnBuildDone += buildDone;
 
-            _extenderProvider = _extenderProvider ?? new AutoT4ExtenderProvider(_dte);
-            _extenderProviderCookies.Add(_objectExtenders.RegisterExtenderProvider(catId, name, _extenderProvider));
+            return Tuple.Create(buildBegin, buildDone);
         }
 
-        private void OnBuildBegin(vsBuildScope scope, vsBuildAction action)
+        private void OnBuildBegin(DTE dte, vsBuildScope scope)
         {
-            RunTemplates(scope, RunOnBuild.BeforeBuild, Options.RunOnBuild == DefaultRunOnBuild.BeforeBuild);
+            RunTemplates(dte, scope, RunOnBuild.BeforeBuild, Options.RunOnBuild == DefaultRunOnBuild.BeforeBuild);
         }
 
-        private void OnBuildDone(vsBuildScope scope, vsBuildAction action)
+        private void OnBuildDone(DTE dte, vsBuildScope scope)
         {
-            RunTemplates(scope, RunOnBuild.AfterBuild, Options.RunOnBuild == DefaultRunOnBuild.AfterBuild);
+            RunTemplates(dte, scope, RunOnBuild.AfterBuild, Options.RunOnBuild == DefaultRunOnBuild.AfterBuild);
         }
 
-        private void RunTemplates(vsBuildScope scope, RunOnBuild buildEvent, bool runIfDefault)
+        private void RunTemplates(DTE dte, vsBuildScope scope, RunOnBuild buildEvent, bool runIfDefault)
         {
-            _dte.GetProjectsWithinBuildScope(scope)
+            dte.GetProjectsWithinBuildScope(scope)
                 .FindT4ProjectItems()
                 .ThatShouldRunOn(buildEvent, runIfDefault)
                 .ToList()
                 .ForEach(item => item.RunTemplate());
         }
 
-        private void RunTemplates(object sender, EventArgs e)
+        private void RunTemplates(DTE dte)
         {
-            _dte.GetProjectsWithinBuildScope(vsBuildScope.vsBuildScopeSolution)
+            dte.GetProjectsWithinBuildScope(vsBuildScope.vsBuildScopeSolution)
                 .FindT4ProjectItems()
                 .ToList()
                 .ForEach(item => item.RunTemplate());
@@ -100,13 +100,10 @@ namespace BennorMcCarthy.AutoT4
         protected override int QueryClose(out bool canClose)
         {
             int result = base.QueryClose(out canClose);
-            if (!canClose)
-                return result;
-
-            if (_buildEvents != null)
+            if (canClose && _dte != null && _buildEvents != null)
             {
-                _buildEvents.OnBuildBegin -= OnBuildBegin;
-                _buildEvents.OnBuildDone -= OnBuildDone;
+                _dte.Events.BuildEvents.OnBuildBegin -= _buildEvents.Item1;
+                _dte.Events.BuildEvents.OnBuildDone -= _buildEvents.Item2;
                 _buildEvents = null;
             }
             return result;
